37ae8800c12f54bf3aa6719ec480e3ab1a7f4815
Riley <rileyp.mckinley@gmail.com>
2023-12-05 17:18:56 -0500

    Moved lab 7 from local to remote

---
+++ BooksREST/src/main/java/restService/BookService.java
@@ -0,0 +1,88 @@
+package restService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.json.JSONObject;
+
+import dao.BookDAO;
+import dao.BookDAOImpl;
+import model.Book;
+import model.Category;
+
+@Path("Books") // this is the path of the service
+public class BookService {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Book> getBooks() {
+		
+		List<Book> bookList = null;
+		
+		try {
+			// calling DAO method to retrieve a list of all books 
+			BookDAO bd = new BookDAOImpl();
+			bookList = bd.findAllBooks();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return bookList;
+	}
+	
+	@Path("/category")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Category> getCategories() {
+		
+		BookDAO bookDao = new BookDAOImpl();
+		List<Category> categoryList = bookDao.findAllCategories();
+		return categoryList;
+	}
+	
+	@Path("/searchByCat/{catid}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Book> searchByCat(@PathParam("catid") String catid) {
+		
+		List<Book> bookList = null;
+		
+		try {
+			// calling DAO method to search book by catetory 
+			BookDAO bd = new BookDAOImpl();
+			
+			bookList = bd.findBooksByCategory(catid);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return bookList;
+	}
+	
+	@Path("/searchByKey/{keyword}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Book> searchByKey(@PathParam("keyword") String keyword) {
+		
+		List<Book> bookList = null;
+		
+		try {
+			// calling DAO method to search book by catetory 
+			BookDAO bd = new BookDAOImpl();
+			
+			bookList = bd.searchBooksByKeyword(keyword);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return bookList;
+	}
+}
